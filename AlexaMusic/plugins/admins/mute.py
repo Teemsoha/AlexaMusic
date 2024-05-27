a4a295940f1a84e443a6ff04fd3fb7374e46c942
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-05-27 12:27:19 +0300

    Update mute.py

--- AlexaMusic/plugins/admins/mute.py
+++ AlexaMusic/plugins/admins/mute.py
@@ -19,12 +19,12 @@ from AlexaMusic import app
 from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils.database import is_muted, mute_on
 from AlexaMusic.utils.decorators import AdminRightsCheck
-
+from AlexaMusic.plugins.play.filters import command
 # Commands
 MUTE_COMMAND = get_command("MUTE_COMMAND")
 
 
-@app.on_message(filters.command(MUTE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(command(MUTE_COMMAND) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
