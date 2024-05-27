b83ecab990aaf4efbc2624f909b651846816e269
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-05-27 12:27:55 +0300

    Update shuffle.py

--- AlexaMusic/plugins/admins/shuffle.py
+++ AlexaMusic/plugins/admins/shuffle.py
@@ -20,12 +20,12 @@ from strings import get_command
 from AlexaMusic import app
 from AlexaMusic.misc import db
 from AlexaMusic.utils.decorators import AdminRightsCheck
-
+from AlexaMusic.plugins.play.filters import command
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
 
 
-@app.on_message(filters.command(SHUFFLE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(command(SHUFFLE_COMMAND) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
     if not len(message.command) == 1:
